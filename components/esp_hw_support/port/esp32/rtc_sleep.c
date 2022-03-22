c927e63a1912fe25dd7b3f5cd0a62dcffceae96a
David OSullivan <dave.p.osullivan@gmail.com>
2022-03-22 13:10:18 +1000

    Don't disable 8M clock when entering deep sleep to fix issue waking from deep sleep

--- components/esp_hw_support/port/esp32/rtc_sleep.c
+++ components/esp_hw_support/port/esp32/rtc_sleep.c
@@ -185,13 +185,14 @@ void rtc_sleep_init(rtc_sleep_config_t cfg)
     } else {
         REG_CLR_BIT(RTC_CNTL_CLK_CONF_REG, RTC_CNTL_CK8M_FORCE_PU);
     }
+    // DPO: This change prevents the 8M oscillator from getting powered down in deep sleep.
     //Keep the RTC8M_CLK on in light_sleep mode if the ledc low-speed channel is clocked by RTC8M_CLK.
-    if (!cfg.int_8m_pd_en && GET_PERI_REG_MASK(RTC_CNTL_CLK_CONF_REG, RTC_CNTL_DIG_CLK8M_EN_M)) {
+    //if (!cfg.int_8m_pd_en && GET_PERI_REG_MASK(RTC_CNTL_CLK_CONF_REG, RTC_CNTL_DIG_CLK8M_EN_M)) {
         REG_CLR_BIT(RTC_CNTL_CLK_CONF_REG, RTC_CNTL_CK8M_FORCE_PD);
         REG_SET_BIT(RTC_CNTL_CLK_CONF_REG, RTC_CNTL_CK8M_FORCE_PU);
-    } else {
-        REG_CLR_BIT(RTC_CNTL_CLK_CONF_REG, RTC_CNTL_CK8M_FORCE_PU);
-    }
+    //} else {
+    //    REG_CLR_BIT(RTC_CNTL_CLK_CONF_REG, RTC_CNTL_CK8M_FORCE_PU);
+    //}
 
     /* enable VDDSDIO control by state machine */
     REG_CLR_BIT(RTC_CNTL_SDIO_CONF_REG, RTC_CNTL_SDIO_FORCE);
